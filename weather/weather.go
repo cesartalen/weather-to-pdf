326c1c570ad87b24cd15adeed3345b561c472696
cesartalen <cesartalense@gmail.com>
2024-04-11 14:43:56 +0200

    Add structs for ForecastData and WeatherResponse, unmarshal into WeatherResponse type and return of this type

--- weather/weather.go
+++ weather/weather.go
@@ -1,13 +1,24 @@
 package weather
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
-func FetchWeather(latitude float64, longitude float64) {
+type ForecastData struct {
+	Time          []string  `json:"time"`
+	Temperature2m []float64 `json:"temperature_2m"`
+}
+
+type WeatherResponse struct {
+	TimezoneAbbrev string       `json:"timezone_abbreviation"`
+	Hourly         ForecastData `json:"hourly"`
+}
+
+func FetchWeather(latitude float64, longitude float64) WeatherResponse {
 	url := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&hourly=temperature_2m", latitude, longitude)
 
 	resp, err := http.Get(url)
@@ -25,5 +36,11 @@ func FetchWeather(latitude float64, longitude float64) {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(body))
+	var weatherResponse WeatherResponse
+	err = json.Unmarshal(body, &weatherResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return weatherResponse
 }
