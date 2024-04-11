ca2dbec4e191cc8ce053c2664bb944e085375111
cesartalen <cesartalense@gmail.com>
2024-04-11 20:20:45 +0200

    Add command-line arguments support, specify city as argument

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"cesartalen/weather-to-pdf/pdf"
 	"cesartalen/weather-to-pdf/weather"
 	"log"
+	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Use param <city>")
+	}
+	city := os.Args[1]
+
 	var locationData weather.Location
 	var weatherData weather.WeatherResponse
 
-	locationData = weather.FetchLocation("Visby")
+	locationData = weather.FetchLocation(city)
 	weatherData = weather.FetchWeather(locationData.Latitude, locationData.Longitude)
 
 	err := pdf.GeneratePDF("weather.pdf", weatherData)
