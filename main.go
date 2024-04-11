9bb65817ce98d65c14be58fae31bfc2c50da8898
cesartalen <cesartalense@gmail.com>
2024-04-11 14:45:10 +0200

    Add use of fetch functions in main, pass data to GeneratePDF function

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"cesartalen/weather-to-pdf/pdf"
+	"cesartalen/weather-to-pdf/weather"
 	"log"
 )
 
 func main() {
-	err := pdf.GeneratePDF("weather.pdf")
+	var locationData weather.Location
+	var weatherData weather.WeatherResponse
+
+	locationData = weather.FetchLocation("Visby")
+	weatherData = weather.FetchWeather(locationData.Latitude, locationData.Longitude)
+
+	err := pdf.GeneratePDF("weather.pdf", weatherData)
 	if err != nil {
 		log.Fatal(err)
 	}
