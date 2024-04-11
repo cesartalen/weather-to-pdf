44729a40b28f70f5030fe9e6713e26ccf5e8e47d
cesartalen <cesartalense@gmail.com>
2024-04-11 13:35:06 +0200

    Add params to FetchWeather, Add url that formats to use params for fetch

--- weather/weather.go
+++ weather/weather.go
@@ -7,8 +7,10 @@ import (
 	"net/http"
 )
 
-func FetchWeather() {
-	resp, err := http.Get("https://api.open-meteo.com/v1/forecast?latitude=59.3294&longitude=18.0687&hourly=temperature_2m")
+func FetchWeather(latitude float64, longitude float64) {
+	url := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&hourly=temperature_2m", latitude, longitude)
+
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
