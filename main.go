6b9193982b472e7204aa8c050235072a8324a527
cesartalen <cesartalense@gmail.com>
2024-04-11 10:54:35 +0200

    Init, trying maroto

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/johnfercher/maroto/v2/pkg/core"
+
+	"github.com/johnfercher/maroto/v2/pkg/components/col"
+
+	"github.com/johnfercher/maroto/v2"
+
+	"github.com/johnfercher/maroto/v2/pkg/components/text"
+)
+
+func main() {
+	m := GetMaroto()
+	document, err := m.Generate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = document.Save("weather.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetMaroto() core.Maroto {
+	m := maroto.New()
+
+	m.AddRow(20,
+		text.NewCol(4, "Text"),
+	)
+
+	m.AddRow(10, col.New(12))
+
+	m.AddRow(20,
+		text.NewCol(4, "Text"),
+	)
+
+	return m
 }
