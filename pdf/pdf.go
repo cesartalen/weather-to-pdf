041e4a6d20c0c77580bc8586337ec42b47a6c06c
cesartalen <cesartalense@gmail.com>
2024-04-11 20:05:48 +0200

    Add header text, celsius as the unit of measurement, and modified the layout a little bit

--- pdf/pdf.go
+++ pdf/pdf.go
@@ -8,13 +8,19 @@ import (
 
 	"github.com/johnfercher/maroto/v2"
 	"github.com/johnfercher/maroto/v2/pkg/components/text"
+	"github.com/johnfercher/maroto/v2/pkg/config"
+	"github.com/johnfercher/maroto/v2/pkg/consts/align"
 	"github.com/johnfercher/maroto/v2/pkg/core"
 	"github.com/johnfercher/maroto/v2/pkg/props"
 )
 
 func GeneratePDF(filename string, weatherData weather.WeatherResponse) error {
-	m := maroto.New()
+	cfg := config.NewBuilder().WithMargins(10, 15, 10).Build()
+	m := maroto.New(cfg)
 
+	m.AddRow(5, text.NewCol(12, "Weather Forecast", props.Text{Align: align.Right}))
+	m.AddRow(5, text.NewCol(12, time.Now().Format("2006-01-02 15:04"), props.Text{Align: align.Right}))
+	m.AddRow(12, text.NewCol(12, fmt.Sprintf("Timezone: %s", weatherData.TimezoneAbbrev), props.Text{Align: align.Right}))
 	// For every day, divides by 24 to get count of days.
 	for i := 0; i < (len(weatherData.Hourly.Time) / 24); i++ {
 		tempCols := make([]core.Col, 0, 7)
@@ -26,14 +32,14 @@ func GeneratePDF(filename string, weatherData weather.WeatherResponse) error {
 		*/
 		for j := 0; j <= 7; j++ {
 			index := i*24 + j*3
-			tempCols = append(tempCols, text.NewCol(1, fmt.Sprintf("%.1f", weatherData.Hourly.Temperature2m[index])))
+			tempCols = append(tempCols, text.NewCol(1, fmt.Sprintf("%.1f °C", weatherData.Hourly.Temperature2m[index]), props.Text{Left: 20}))
 
 			parsedTime, err := time.Parse("2006-01-02T15:04", weatherData.Hourly.Time[index])
 			if err != nil {
 				log.Fatal("Error parsing time")
 			}
 
-			timeCols = append(timeCols, text.NewCol(1, parsedTime.Format("15:04")))
+			timeCols = append(timeCols, text.NewCol(1, parsedTime.Format("15:04"), props.Text{Left: 14}))
 		}
 
 		parsedTime, err := time.Parse("2006-01-02T15:04", weatherData.Hourly.Time[i*24])
@@ -43,11 +49,12 @@ func GeneratePDF(filename string, weatherData weather.WeatherResponse) error {
 
 		formattedTime := parsedTime.Format("January 02 Monday")
 
-		m.AddRow(10, text.NewCol(10, formattedTime, props.Text{
+		m.AddRow(5, text.NewCol(10, formattedTime, props.Text{
 			Size: 12,
+			Left: 10,
 		}))
 		m.AddRow(5, timeCols...)
-		m.AddRow(10, tempCols...)
+		m.AddRow(20, tempCols...)
 	}
 
 	document, err := m.Generate()
