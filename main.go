1585110ec9626ad6918874361f7f5c431f354ee7
cesartalen <cesartalense@gmail.com>
2024-04-11 11:13:59 +0200

    Refactor pdf creation from main -> pdf/pdf.go

--- main.go
+++ main.go
@@ -1,42 +1,13 @@
 package main
 
 import (
+	"cesartalen/weather-to-pdf/pdf"
 	"log"
-
-	"github.com/johnfercher/maroto/v2/pkg/core"
-
-	"github.com/johnfercher/maroto/v2/pkg/components/col"
-
-	"github.com/johnfercher/maroto/v2"
-
-	"github.com/johnfercher/maroto/v2/pkg/components/text"
 )
 
 func main() {
-	m := GetMaroto()
-	document, err := m.Generate()
+	err := pdf.GeneratePDF("weather.pdf")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	err = document.Save("weather.pdf")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func GetMaroto() core.Maroto {
-	m := maroto.New()
-
-	m.AddRow(20,
-		text.NewCol(4, "Text"),
-	)
-
-	m.AddRow(10, col.New(12))
-
-	m.AddRow(20,
-		text.NewCol(4, "Text"),
-	)
-
-	return m
 }
---
+++ pdf/pdf.go
@@ -0,0 +1,35 @@
+package pdf
+
+import (
+	"log"
+
+	"github.com/johnfercher/maroto/v2"
+	"github.com/johnfercher/maroto/v2/pkg/components/col"
+	"github.com/johnfercher/maroto/v2/pkg/components/text"
+)
+
+func GeneratePDF(filename string) error {
+	m := maroto.New()
+
+	m.AddRow(20,
+		text.NewCol(4, "Text"),
+	)
+
+	m.AddRow(10, col.New(12))
+
+	m.AddRow(20,
+		text.NewCol(4, "Text"),
+	)
+
+	document, err := m.Generate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = document.Save(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
