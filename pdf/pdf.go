748af4b2662d0de8b7a8f17e2892de28d8525d6a
cesartalen <cesartalense@gmail.com>
2024-04-11 17:33:04 +0200

    Add new rows for date, time, temp. All data now fits on one page. Formatted times to be more "user friendly"

--- pdf/pdf.go
+++ pdf/pdf.go
@@ -4,24 +4,47 @@ import (
 	"cesartalen/weather-to-pdf/weather"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/johnfercher/maroto/v2"
 	"github.com/johnfercher/maroto/v2/pkg/components/text"
+	"github.com/johnfercher/maroto/v2/pkg/core"
 )
 
 func GeneratePDF(filename string, weatherData weather.WeatherResponse) error {
 	m := maroto.New()
 
-	for i := 0; i < len(weatherData.Hourly.Time); i++ {
-		if (i % 24) == 0 {
+	// For every day, divides by 24 to get count of days.
+	for i := 0; i < (len(weatherData.Hourly.Time) / 24); i++ {
+		tempCols := make([]core.Col, 0, 7)
+		timeCols := make([]core.Col, 0, 7)
+
+		/* j*3 because 24/8 = 3, dividing day into 8 parts. Loops 8 times for every days worth of data. Should not need to be modified if more days are added.
+		Doing this to make the data fit better on the page.
+		Stores data in two cols so time can be put in a row above temp.
+		*/
+		for j := 0; j <= 7; j++ {
+			index := i*24 + j*3
+			tempCols = append(tempCols, text.NewCol(1, fmt.Sprintf("%.1f", weatherData.Hourly.Temperature2m[index])))
+
+			parsedTime, err := time.Parse("2006-01-02T15:04", weatherData.Hourly.Time[index])
+			if err != nil {
+				log.Fatal("Error parsing time")
+			}
+
+			timeCols = append(timeCols, text.NewCol(1, parsedTime.Format("15:04")))
 		}
 
-		temp := fmt.Sprintf("%.1f", weatherData.Hourly.Temperature2m[i])
+		parsedTime, err := time.Parse("2006-01-02T15:04", weatherData.Hourly.Time[i*24])
+		if err != nil {
+			log.Fatal("Error parsing time")
+		}
 
-		m.AddRow(5,
-			text.NewCol(4, string(weatherData.Hourly.Time[i])),
-			text.NewCol(4, temp),
-		)
+		formattedTime := parsedTime.Format("January 02 Monday")
+
+		m.AddRow(10, text.NewCol(5, formattedTime))
+		m.AddRow(5, timeCols...)
+		m.AddRow(10, tempCols...)
 	}
 
 	document, err := m.Generate()
